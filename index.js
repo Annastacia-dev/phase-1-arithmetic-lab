6bbfc150bd1a59dcd063fe0f9344438d49e92387
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-08-15 17:09:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+const num3 = 16;
+const num4 = 12
+const mod = num3 % num4;
+
+const max = Math.max(3,8,11,14,17,20);
